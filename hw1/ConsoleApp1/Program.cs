ba93d2e1e5fc0234f702a3bc0c05bb6cc4eab644
Natasha Ovchinnikova <blueberric@gmail.com>
2020-12-26 02:42:41 +0300

    I tried so hard and got so far but in the end

---
+++ hw1/ConsoleApp1/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_bannikov
+{
+
+    public class Subunit
+    {
+        public int UnitId;
+        public string UnitName;
+        public List<Subunit> SubUnits;
+        public List<WhiteCollar> Employees;
+        public Subunit()
+        {
+            UnitId = ' ';
+            UnitName = "unknown";
+            SubUnits = new List<Subunit>();
+            Employees = new List<WhiteCollar>();
+        }
+
+        public Subunit(int unitid, string unitname)
+        {
+            UnitId = unitid;
+            UnitName = unitname;
+            SubUnits = new List<Subunit>();
+            Employees = new List<WhiteCollar>();
+        }
+
+        public void AddSubUnit(Subunit sunitname)
+        {
+            SubUnits.Add(sunitname);
+        }
+
+        public void AddEmployee(WhiteCollar employee)
+        {
+            Employees.Add(employee);
+        }
+        public string GetSubunit()
+        {
+            return "Unit Name:" + UnitName;
+        }
+    }
+
+    public class Corporate
+    {
+        public string CorpName;
+        public List<Subunit> Departments;
+
+        public Corporate()
+        {
+            CorpName = "unknown";
+            Departments = new List<Subunit>();
+        }
+
+        public Corporate(string corpname)
+        {
+            CorpName = corpname;
+            Departments = new List<Subunit>();
+        }
+
+        public void AddDepartments(Subunit depname)
+        {
+            Departments.Add(depname);
+        }
+
+    }
+
+    public class WhiteCollar
+    {
+        public string Name, Surname, Position, BirthDate;
+        public bool IsHead;
+        // тк вывод организовывать не нужно, дата рождения строкой 
+        public WhiteCollar()
+        {
+            Name = "unknown";
+            Surname = "unknown";
+            Position = "unknown";
+            BirthDate = "xx.xx.xxxx";
+            IsHead = false;
+        }
+
+        public WhiteCollar(string name, string surname, string position,
+            string birthdate, bool ishead)
+        {
+            Name = name;
+            Surname = surname;
+            Position = position;
+            BirthDate = birthdate;
+            IsHead = ishead;
+        }
+
+        public void SetHead()
+        {
+            IsHead = true;
+        }
+
+        public string GetWhiteCollar() => "Name: " + Name + " Surname:" + Surname + ". Date of Birth:" +
+                    BirthDate + ". Position held: " + Position + "Dep. Head: " + IsHead;
+    }
+
+
+    class Program
+    {
+        public static void PrintUnitInfo(Subunit subun, int indent)
+        {
+            Console.Write(new String(' ', indent));
+            Console.WriteLine(subun.UnitName);
+            indent++;
+            foreach (Subunit depart in subun.SubUnits)
+            {
+                PrintUnitInfo(depart, indent);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Corporate parkwood = new Corporate("Parkwood Entertainment");
+            Subunit marketing = new Subunit(1, "Marketing");
+            Subunit arts = new Subunit(2, "Artists & Repertoire");
+            Subunit brands = new Subunit(3, "Brands");
+            Subunit smed = new Subunit(4, "Social media marketing");
+            Subunit tv = new Subunit(5, "TV marketing");
+            WhiteCollar bey = new WhiteCollar("Beyonce", "Knowles", "CEO", "4/09/1981", true);
+            WhiteCollar jlo = new WhiteCollar("Jennifer", "Lopez", "Singer", "24/07/1969", false);
+            WhiteCollar emgad = new WhiteCollar("Emin", "Gadzhiev", "Marketing specialist", "22/01/2000", false);
+            marketing.AddSubUnit(smed);
+            marketing.AddSubUnit(tv);
+            arts.AddEmployee(bey);
+            arts.AddEmployee(jlo);
+            smed.AddEmployee(emgad);
+            emgad.SetHead();
+            parkwood.AddDepartments(marketing);
+            parkwood.AddDepartments(arts);
+            parkwood.AddDepartments(brands);
+
+            Console.WriteLine(parkwood.CorpName + " Hierarachy");
+
+            foreach (Subunit subunit in parkwood.Departments)
+            {
+                PrintUnitInfo(subunit, 3);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
---
+++ hwpart2/hwpart2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+namespace Serialized_HW1
+{
+    public class Subunit
+    {
+        public int UnitId { get; set; }
+        public string UnitName { get; set; }
+        public List<Subunit> SubUnits { get; set; }
+        public List<WhiteCollar> Employees { get; set; }
+    }
+    public class Corporate
+    {
+        public string CorpName { get; set; }
+        public List<Subunit> Departments { get; set; }
+    }
+
+    public class WhiteCollar
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public string BirthDate { get; set; }
+        public bool IsHead { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Corporate parkwood = new Corporate
+            {
+                CorpName = "Parkwood Entertainment",
+                Departments = new List<Subunit>()
+            };
+
+            Subunit arts = new Subunit
+            {
+                UnitId = 1,
+                UnitName = "Artists & Repertoire",
+                SubUnits = new List<Subunit>(),
+                Employees = new List<WhiteCollar>()
+            };
+
+            Subunit marketing = new Subunit
+            {
+                UnitId = 2,
+                UnitName = "Marketing",
+                SubUnits = new List<Subunit>(),
+                Employees = new List<WhiteCollar>()
+            };
+
+            WhiteCollar bey = new WhiteCollar
+            {
+                Name = "Beyonce",
+                Surname = "Knowles",
+                Position = "CEO",
+                BirthDate = "04/09/1981",
+                IsHead = true
+            };
+
+            string json = JsonSerializer.Serialize(parkwood);
+            Console.WriteLine(json);
+            Console.ReadLine();
+        }
+    }
+}
